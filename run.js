35ea783524854a4086c87f50b878111131c07ec0
Ethan Smith <ethan@onebytegone.com>
2017-03-03 21:50:22 -0500

    Initial construction

--- run.js
+++ run.js
@@ -2,5 +2,25 @@
 
 'use strict';
 
-var App = require('./src/index'),
-    app = new App();  // eslint-disable-line no-unused-vars
+var fs = require('fs'),
+    yaml = require('js-yaml'),
+    App = require('./src/index'),
+    app, doc, output;
+
+app = new App();
+
+try {
+   doc = yaml.safeLoad(fs.readFileSync('demo.yml', 'utf8'));  // eslint-disable-line no-sync
+
+   output = app.generate(
+      doc.rows,
+      {
+         title: doc.title,
+         header: doc.header,
+      }
+   );
+
+   console.log(output);  // eslint-disable-line no-console
+} catch(e) {
+   console.log(e);  // eslint-disable-line no-console
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,65 @@
 'use strict';
 
-var Class = require('class.extend');
+var _ = require('underscore'),
+    Class = require('class.extend'),
+    nunjucks = require('nunjucks'),
+    moment = require('moment');
 
 module.exports = Class.extend({
 
-   init: function() {
-      console.log('Γεια σου!');  // eslint-disable-line no-console
+   generate: function(rows, options) {
+      var self = this,
+          headerKeys, header;
+
+      options = _.extend({
+         templateRoot: './templates',
+         template: 'basic-table.html'
+      }, options);
+
+      headerKeys = this.findAllKeysUsed(rows);
+      header = _.map(headerKeys, function(value) {
+         return options.header[value] || value;
+      });
+      header = _.map(header, this.upgradeToValueObjectIfNeeded);
+
+      rows = _.map(rows, function(values) {
+         return self.generateRow(values, headerKeys);
+      });
+
+      nunjucks.configure(options.templateRoot, { autoescape: true });
+
+      return nunjucks.render(options.template, {
+         title: options.title,
+         header: header,
+         rows: rows,
+         timestamp: moment().format('Y-MM-DD h:mma')
+      });
+   },
+
+   findAllKeysUsed: function(objects) {
+      return _.reduce(objects, function(memo, object) {
+         return _.union(memo, _.keys(object));
+      }, []);
+   },
+
+   generateRow: function(values, columnKeys) {
+      var self = this;
+
+      return {
+         values: _.map(columnKeys, function(key) {
+            return self.upgradeToValueObjectIfNeeded(values[key] || '');
+         })
+      };
+   },
+
+   upgradeToValueObjectIfNeeded: function(value) {
+      if (_.isObject(value)) {
+         return value;
+      }
+
+      return {
+         value: value
+      };
    }
+
 });
